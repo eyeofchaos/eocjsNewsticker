ec8757f738dcf6ca7c09069200a8152ea078a519
Dieter Schmitt <dieterschmitt75@icloud.com>
2019-08-17 04:27:16 +0200

    Complete Update

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# eocNewsticker
-A jQuery newsticker plugin
+# eocNewsticker: A jQuery newsticker plugin
+
+## Requirements
+
+jQuery 2.2.4 or higher
+
+## Usage
+
+1. Include the provided JS/CSS files
+
+```html
+<script src="eoc-newsticker.js"></script>
+<link rel="stylesheet" href="eoc-newsticker.css">
+```
+
+2. Write your HTML Content (only for static use)
+
+```html
+<div id="example">The quick brown fox jumps over the lazy dog</div>
+```
+
+3. Invoke the newsticker on your selected HTML Tag
+
+```javascript
+$(function() {
+  $("#example").eocNewsticker();
+});
+```
+
+4. Options
+
+    * speed: The time it takes (in seconds) to move the text 1000px from right to left (normalized)
+    * timeout: The time the slider waits after domready, before starting to run
+    * divider: The signs used as a divider between the text blocks (if the text is not long enough to fill the whole width)
+    * type (static or ajax): Regular usage (static) or get contents with ajax as a json file (ajax)
+    * interval: AJAX update interval in seconds
+
+## Future Development
+
+I intend to read the news from a JSON file. Therefore the newsticker will be updated on-the-fly in a constant interval.
+
+## Notice
+
+Please bear in mind, that this project is in a very early stage of development, so please have mercy with me :)
+
+## License
+
+Released under the MIT license - https://opensource.org/licenses/MIT
--- eoc-newsticker.js
+++ eoc-newsticker.js
@@ -9,17 +9,25 @@
     // _____ Options _____
 
     let defaults = {
-      speed: 20,
-      timeout: 2,
-      divider: '+++'
+      speed:    20,
+      timeout:  2,
+      divider:  '+++',
+      type:     'static', // static or ajax
+      interval: 600       // re-fetch ajax source (seconds)
     };
     let settings = $.extend({}, defaults, options);
 
     // _______ Inner Variables _______
 
-    let element = this;
+    let self = this;
+    let content = self.html();
     let container = {};
-    let inner = {};
+    let one = {};
+    let two = {};
+    let both = {};
+    let oneNeedsUpdate = false;
+    let twoNeedsUpdate = false;
+    let windowWidth = $(window).width();
 
     // _______ Init _______
 
@@ -32,21 +40,13 @@
 
     function create() {
 
-      element.addClass('eoc-newsticker').html('<div><div>' + element.html() + ' ' + settings.divider + '</div></div>');
-      container = element.find('> div');
-      inner = element.find('> div > div');
+      self.addClass('eoc-newsticker').html('<div class="eoc-newsticker-container"><div class="eoc-newsticker-loader" style="display: none;"></div><div class="eoc-newsticker-one"></div><div class="eoc-newsticker-two"></div></div>');
 
-      let content = inner.html();
-      let containerWidth = container.width();
-      let innerWidth = inner.width();
-
-      while (containerWidth > innerWidth) {
-        inner.append(' ' + content);
-        innerWidth = inner.width();
-      }
-
-      inner.append('&nbsp;');
-      container.append(container.html());
+      container = self.find('.eoc-newsticker-container');
+      one = self.find('.eoc-newsticker-one');
+      // one.css('background', 'green');
+      two = self.find('.eoc-newsticker-two');
+      both = self.find('.eoc-newsticker-one, .eoc-newsticker-two');
 
     }
 
@@ -54,39 +54,115 @@
 
     function start() {
 
+      if (settings.type === 'static') {
+        content = content + ' ' + settings.divider;
+        run(content, (settings.timeout * 1000));
+      } else if (settings.type === 'ajax') {
+        self.find('.eoc-newsticker-loader').show();
+        // Show spinner
+        // Get content from AJAX
+        // Then run(content, 0);
+      }
+
+    }
+
+    // _______ Run _______
+
+    function run(content, timeout) {
+
+      update(both, content);
+      two.css({'left': one.width()});
+
       setTimeout(function() {
 
-        let inner = element.find('> div > div');
-        let width = inner.width();
+        let width = one.width();
         let speed = settings.speed * width;
-        let slideOne = inner.eq(0);
-        let slideTwo = inner.eq(1).css({'left': width});
-        animateSlide(slideOne, 0, -width, width, speed);
-        animateSlide(slideTwo, width, 0, width, speed);
 
-      }, (settings.timeout * 1000));
+        animateSlide(one, 0, -width, speed);
+        animateSlide(two, width, 0, speed);
+
+      }, timeout);
+
+    }
+
+    // _______ Update _______
+
+    function update(slide, content) {
+
+      slide.html(content);
+      while (container.width() > slide.width()) {
+        slide.append(' ' + content);
+      }
+      slide.append('&nbsp;');
 
     }
 
     // _______ Animation _______
 
-    function animateSlide(slide, start, destination, width, speed) {
+    function animateSlide(slide, start, destination, speed) {
 
       slide.animate(
         {left: destination},
         speed,
         'linear',
         function() {
+
+          let width;
+
           if (start === 0) {
+
+            if (slide === one && oneNeedsUpdate) {
+              // console.log('UPDATE ONE REGULAR');
+              update(one, content);
+              oneNeedsUpdate = false;
+            } else if (slide === two && twoNeedsUpdate) {
+              // console.log('UPDATE TWO REGULAR');
+              update(two, content);
+              twoNeedsUpdate = false;
+            }
+
+            slide === one ? width = two.width() : width = one.width();
+            speed = settings.speed * width;
             slide.css({'left': width});
-            animateSlide(slide, width, 0, width, speed);
+            animateSlide(slide, width, 0, speed);
+
           } else {
-            animateSlide(slide, 0, -width, width, speed);
+
+            slide === one ? width = one.width() : width = two.width();
+            speed = settings.speed * width;
+            animateSlide(slide, 0, -width, speed);
+
           }
+
         }
       );
 
     }
+
+    // _______ Resize _______
+
+    $(window).on('resize', function() {
+      if ($(window).width() != windowWidth) {
+
+        if ($(window).width() > windowWidth) {
+          if (one.position().left > 0) {
+            // console.log('UPDATE ONE IMMEDIATELY');
+            update(one, content);
+            twoNeedsUpdate = true;
+          } else if (two.position().left > 0) {
+            // console.log('UPDATE TWO IMMEDIATELY');
+            update(two, content);
+            oneNeedsUpdate = true;
+          }
+        } else {
+          oneNeedsUpdate = true;
+          twoNeedsUpdate = true;
+        }
+
+        windowWidth = $(window).width();
+
+      }
+    });
 
     // _______ Init _______
 
