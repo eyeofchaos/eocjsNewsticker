f71dce3c7585d86bba5acf21d50dc81b40429a2a
Dieter Schmitt <39838339+eyeofchaos@users.noreply.github.com>
2025-10-26 17:45:39 +0100

    Update

--- example-json.php
+++ example-json.php
@@ -1,4 +1,4 @@
 <?php
-header('Content-type: application/json; charset=utf-8');
+header('Content-type: application/json; charset=UTF-8');
 echo '{"1": "This text will be updated every 30 seconds on-the-fly!", "2": "Last update: ' . date('Y-m-d h:i:s a') . '"}';
 ?>
